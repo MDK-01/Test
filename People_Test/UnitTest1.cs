d71df367ead6cf60163de4196265b20036cd92f6
Хвастунов <op.ap.191@mail.ru>
2021-05-17 11:18:35 +0300

    Добавьте файлы проекта.

---
+++ People_Test/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using AutotestApplicationExample;
+
+namespace People_Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        People PeopleTestTask = new People("Hvastunov", "admin", "exeee@mail.ru", "8(555)335-35-35");
+
+        [TestMethod]
+        public void TestMethodLogin()
+        {
+            Assert.AreEqual("HvastunovLogin", PeopleTestTask.login1);
+        }
+        [TestMethod]
+        public void TestMethodPassword()
+        {
+            Assert.AreEqual("admin", PeopleTestTask.password1);
+        }
+        [TestMethod]
+        public void TestMethodEmail()
+        {
+            Assert.AreEqual("exeee@mail.ru", PeopleTestTask.email1);
+        }
+        [TestMethod]
+        public void TestMethodPhone()
+        {
+            Assert.AreEqual("8(555)335-35-35", PeopleTestTask.phone1);
+        }
+    }
+}
